b4a575b2d518a36d509f55d57405c96a789bc166
VISHWA-VIDYA\vivek <vivekyadav7875.vy@gmail.com>
2026-06-04 13:43:26 +0530

    Add Home page text

---
+++ src/MenuTabs/Home.tsx
@@ -0,0 +1,26 @@
+import { Col, Row } from "antd";
+
+const Home = () => {
+    return <Col className="w-full h-screen flex items-center justify-center overflow-hidden">
+        <Row className={'w-full'}>
+            <Col className={'w-1/2 flex flex-col items-center justify-center'}>
+                <img src="/../src/assets/vivek.png" alt="Vivekraj" className="mb-4 h-3/5" />
+            </Col>
+            <Col className={'w-1/2 flex flex-col items-center justify-center'}>
+                <h1 className="text-8xl font-black">
+                    <b>Turning Vision Into</b><br />
+                    <b>Reality With Code</b><br />
+                    <b> And Design.</b>
+                </h1>
+                <p className="text-lg mt-4 font-mono text-justify px-10 font-bold">
+                    As a skilled Frontend React js developer, <br />
+                    I am dedicated to turning ideas
+                    into innovative web applications. Explore my lateset projects and articles,
+                    showcasing my expertise in React js and web development.
+                </p>
+            </Col>
+        </Row >
+    </Col >
+}
+
+export default Home;
