f6e221e148a6fc89d3095946fd21e0230cf035a6
VISHWA-VIDYA\vivek <vivekyadav7875.vy@gmail.com>
2026-06-13 23:55:29 +0530

    Update code for theme implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import { Row } from "antd";
-// import { LinkedinOutlined } from "@ant-design/icons";
+import { Button, Col, ConfigProvider, theme } from "antd";
 import MenuTabs from "./MenuTabs/MenuTabs";
+import { useTheme } from "./ThemeSelect/ThemeContext";
 
-export default function App() {
+const { darkAlgorithm, defaultAlgorithm } = theme;
+
+const App = () => {
+  const { darkMode, toggleTheme } = useTheme();
+
   return (
-    <Row className={"h-screen m-2 overflow-hidden"}>
-      <MenuTabs />
-    </Row>
+    <ConfigProvider
+      theme={{ algorithm: darkMode ? darkAlgorithm : defaultAlgorithm }}
+    >
+      <Col style={{ background: darkMode ? "#0000" : "#faf7f7" }}>
+        <MenuTabs />
+        <Button onClick={toggleTheme} color={darkMode ? "green" : "red"}>
+          Theme Button
+        </Button>
+      </Col>
+    </ConfigProvider>
   );
-}
+};
+
+export default App;
