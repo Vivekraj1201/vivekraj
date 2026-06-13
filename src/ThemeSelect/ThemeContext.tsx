d1c0353eb32840df29c67dad19d97cd212921f15
VISHWA-VIDYA\vivek <vivekyadav7875.vy@gmail.com>
2026-06-13 23:58:04 +0530

    Created Theme Context for Context API code in app to toggle theme

--- src/ThemeSelect/ThemeContext.tsx
+++ src/ThemeSelect/ThemeContext.tsx
@@ -1,28 +1,50 @@
-import { createContext, type ReactNode } from "react";
-
-interface Theme {
-    primaryColor: string;
-    secondaryColor: string;
+import {
+    createContext,
+    useContext,
+    useState
+} from "react";
+import type { ReactNode } from "react";
+interface ThemeContextType {
+    darkMode: boolean;
+    toggleTheme: () => void;
+    setDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
 }
+
+const ThemeContext = createContext<ThemeContextType | undefined>(
+    undefined
+);
 
 interface ThemeProviderProps {
     children: ReactNode;
 }
 
-export const ThemeContext = createContext<Theme>({
-    primaryColor: "#1677ff",
-    secondaryColor: "#ff4d4f",
-});
+export const ThemeProvider = ({
+    children,
+}: ThemeProviderProps) => {
+    const [darkMode, setDarkMode] = useState<boolean>(false);
 
-export const ThemeProvider = ({ children }: ThemeProviderProps) => {
-    const theme: Theme = {
-        primaryColor: "#1677ff",
-        secondaryColor: "#ff4d4f",
+    const toggleTheme = () => {
+        setDarkMode((prev) => !prev);
     };
 
     return (
-        <ThemeContext.Provider value={theme}>
+        <ThemeContext.Provider
+            value={{
+                darkMode,
+                toggleTheme,
+                setDarkMode,
+            }}
+        >
             {children}
         </ThemeContext.Provider>
     );
+};
+
+export const useTheme = (): ThemeContextType => {
+    const context = useContext(ThemeContext);
+
+    if (!context) {
+        throw new Error("useTheme must be used within a ThemeProvider");
+    }
+    return context;
 };
