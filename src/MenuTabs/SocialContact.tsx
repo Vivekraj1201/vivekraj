fcde1d59e292c9cd30359b58232de0f555e67035
VISHWA-VIDYA\vivek <vivekyadav7875.vy@gmail.com>
2026-06-16 22:58:07 +0530

    Clean code

--- src/MenuTabs/SocialContact.tsx
+++ src/MenuTabs/SocialContact.tsx
@@ -8,20 +8,10 @@ import { useTheme } from "../ThemeSelect/ThemeContext";
 const SocialContact = () => {
     const { darkMode, toggleTheme } = useTheme();
     return (
-        <Col
-            style={{
-                position: "absolute",
-                top: 12,
-                right: 24,
-                display: "flex",
-                gap: 20,
-                fontSize: 22,
-            }}
-        >
+        <Col style={{ position: "absolute", top: 12, right: 24, display: "flex", gap: 20, fontSize: 22 }}>
             <a href="https://wa.me/918308120194" target="_blank">
                 <WhatsAppOutlined style={{ color: '#0fc765' }} />
             </a>
-
             <a href="https://instagram.com/vivekraj_1201" >
                 <InstagramOutlined style={{ color: '#E72D88' }} />
             </a>
