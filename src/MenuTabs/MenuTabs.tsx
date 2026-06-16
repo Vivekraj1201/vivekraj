aa1d9bc57b7319aecaa3a0cafb8bd531fcd26801
VISHWA-VIDYA\vivek <vivekyadav7875.vy@gmail.com>
2026-06-16 22:58:26 +0530

    Added Project component

--- src/MenuTabs/MenuTabs.tsx
+++ src/MenuTabs/MenuTabs.tsx
@@ -1,18 +1,18 @@
 import { Col, Tabs } from 'antd';
-import Home from './Home';
+import Home from './Menu/Home';
 import SocialContact from './SocialContact';
-
+import Projects from './Menu/Projects';
 const MenuTabs = () => {
     const items = [
         { key: "1", label: <b>Home</b>, children: <Home /> },
-        { key: "2", label: <b>Projects</b>, children: 'project' },
+        { key: "2", label: <b>Projects</b>, children: <Projects /> },
         { key: "3", label: <b>Contact</b>, children: 'contact' },
         { key: "4", label: <b>About</b>, children: 'about' },
 
     ];
 
     return (
-        <Col style={{ position: "relative" }}>
+        <Col style={{ position: "relative", margin: "12px" }}>
             <Tabs items={items} />
             <SocialContact />
         </Col>
